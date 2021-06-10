2baca4d59a7a7c2fe7ee5f68efde5f06dc27e4b3
haripriyamanne <haripriyamanne4@gmail.com>
2021-06-10 21:31:44 +0530

    Add project files.

---
+++ UnitTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UnitTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
